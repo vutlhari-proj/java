7a8a6a940a64794c5ae3305b00590a92de3623a4
= <=>
2025-08-17 14:38:57 +0200

    sending student to the db

--- src/main/resources/static/scripts/student/student.js
+++ src/main/resources/static/scripts/student/student.js
@@ -14,7 +14,30 @@ export class Student{
 
 export let students = [];
 export const studentFunctions = {
-  
+  student: null,
+
+  async addStudent(studDetails){
+    this.student = new Student(studDetails);
+
+    try {
+    const response = await fetch("http://localhost:8080/api/students", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(this.student)
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    alert("Student added")
+
+    } catch (err) {
+      alert("Student addition failed");
+    }
+  },
+
   getStudent(studNum){
     return students.find((student) => student.studNum == studNum);
   }
