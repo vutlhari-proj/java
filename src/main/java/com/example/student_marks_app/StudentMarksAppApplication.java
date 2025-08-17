b2edc282feba02d5eeac54d332a75a00f03b7ffa
= <=>
2025-08-17 15:19:40 +0200

    Launching webserver on port 8081

--- src/main/java/com/example/student_marks_app/StudentMarksAppApplication.java
+++ src/main/java/com/example/student_marks_app/StudentMarksAppApplication.java
@@ -1,13 +1,38 @@
 package com.example.student_marks_app;
 
+import java.awt.Desktop;
+import java.net.URI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class StudentMarksAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentMarksAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentMarksAppApplication.class, args);
+    }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+
+        if (event.getApplicationContext() instanceof WebServerApplicationContext webContext) {
+            int port = webContext.getWebServer().getPort();
+
+            String url = "http://localhost:" + port;
+
+            if (Desktop.isDesktopSupported()) {
+                try {
+                    Desktop.getDesktop().browse(new URI(url));
+                } catch (Exception e) {
+                    System.err.println("Failed to open browser: " + e.getMessage());
+                }
+            } else {
+                System.out.println("open browser to " + url);
+            }
+        }
+
+    }
 }
