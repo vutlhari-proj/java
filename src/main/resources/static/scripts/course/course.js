c71e900136cdd797af2088be7734af7e725f4083
= <=>
2025-08-19 21:34:30 +0200

    adding course functionality

---
+++ src/main/resources/static/scripts/course/course.js
@@ -0,0 +1,70 @@
+export class Course{
+  code;
+  courseName;
+  modules;
+
+  constructor(courseDetails){
+    this.code = courseDetails.code;
+    this.courseName = courseDetails.courseName;
+    this.modules = courseDetails.modules || null;
+  }
+}
+
+export let courses = [];
+export const courseFunctions = {
+  course: null,
+
+  async addCourse(courseDetails){
+    try{
+      this.course = new Course(courseDetails);
+    
+      const response = await fetch("/api/courses", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(this.course)
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const savedCourse = await response.json();
+      courses.push(savedCourse);
+
+      document.querySelectorAll("tr.input-row").forEach(row => row.remove());
+
+      document.querySelector(".js-body")
+      .innerHTML += 
+      `
+        <tr>
+          <td>${savedCourse.code}</td>
+          <td>${savedCourse.courseName}</td>
+        </tr>
+      `
+    }
+    catch(error){
+      alert("Could not add course");
+    }
+  },
+
+  async loadCourses(){
+    const res = await fetch("/api/courses");
+    const coursesJson = await res.json();
+
+    courses = coursesJson.map((course) => new Course(course));
+
+    let courseHtml = ``;
+    courses.forEach((course) =>{
+      courseHtml += 
+      `
+        <tr>
+          <td>${course.code}</td>
+          <td>${course.courseName}</td>
+        <tr/>
+      `;
+    });
+
+    document.querySelector(".js-body")
+    .innerHTML = courseHtml;
+  }
+}
---
+++ src/main/resources/static/scripts/courses.js
@@ -0,0 +1,43 @@
+import { Course, courseFunctions, courses } from "./course/course.js";
+courseFunctions.loadCourses();
+
+document.querySelector(".add-course-img").addEventListener("click", ()=>{
+  render.newCourse();
+
+  const code = document.getElementById("code");
+  code.focus();
+
+  const name = document.getElementById("name");
+
+  code.addEventListener("keydown", (e)=>{
+    if (e.key == "Enter" && code.value.trim() != null ) {
+      code.blur();
+      name.focus();
+    }
+  });
+
+  name.addEventListener("keydown", (e)=>{
+    if (e.key == "Enter" && name.value.trim() != null ) {
+      courseFunctions.addCourse({code: code.value.toUpperCase(), courseName: name.value});
+    }
+  });
+
+});
+
+const render = {
+  newCourse(){
+    document.querySelector(".js-body")
+    .innerHTML +=
+    `
+      <tr class="input-row">
+        <td>
+          <input type="text" id="code" placeholder="course code..." autocomplete="off" required>
+        </td>
+
+        <td>
+          <input type="text" id="name" placeholder="course name..." autocomplete="off" required>
+        </td>
+      </tr>
+    `;
+  }
+}
